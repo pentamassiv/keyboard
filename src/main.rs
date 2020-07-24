4cd9de3639f2ad1ada7a6515701d0e9c33966de2
pentamassiv <pentamassiv@posteo.de>
2020-07-24 15:54:05 +0200

    added time to dots, only draw dots within time

--- src/main.rs
+++ src/main.rs
@@ -36,19 +36,21 @@ use relm_derive::Msg;
 
 use self::Msg::*;
 
-// Defines color of path
+// Defines const for drawn path
 const PATHCOLOR: (f64, f64, f64, f64) = (0.105, 0.117, 0.746, 0.9);
-const PATHLENGTH: usize = 10;
+const PATHLENGTH: usize = 100;
 const PATHWIDTH: f64 = 4.5;
+const PATHFADINGTIME: u32 = 400;
 
 #[derive(Clone)]
 struct Dot {
     position: (f64, f64),
+    time: u32,
 }
 
 impl Dot {
-    fn generate(position: (f64, f64)) -> Self {
-        Dot { position }
+    fn generate(position: (f64, f64), time: u32) -> Self {
+        Dot { position, time }
     }
 }
 
@@ -62,7 +64,7 @@ pub struct Model {
 pub enum Msg {
     Press,
     Release,
-    MovePointer((f64, f64)),
+    MovePointer((f64, f64), u32),
     Quit,
     UpdateDrawBuffer,
 }
@@ -128,7 +130,7 @@ impl Widget for Win {
                 },
                 #[name="overlay"]
                 gtk::Overlay {
-                    motion_notify_event(_, event) => (MovePointer(event.get_position()), Inhibit(false)),
+                    motion_notify_event(_, event) => (MovePointer(event.get_position(), event.get_time()), Inhibit(false)),
                     button_press_event(_, event) => (Press, Inhibit(false)),
                     button_release_event(_, event) => (Release, Inhibit(false)),
                     draw(_, _) => (UpdateDrawBuffer, Inhibit(false)),
@@ -171,9 +173,9 @@ impl Widget for Win {
                 self.model.dots = Vec::new();
                 //self.model.draw_handler.get_context().show_page();
             }
-            MovePointer(pos) => {
+            MovePointer(pos, time) => {
                 if self.model.is_pressed {
-                    self.model.dots.push(Dot::generate(pos));
+                    self.model.dots.push(Dot::generate(pos, time));
                 }
             }
             Quit => gtk::main_quit(),
@@ -195,8 +197,16 @@ impl Widget for Win {
         self.erase_path();
         context.set_operator(cairo::Operator::Over);
         context.set_source_rgba(PATHCOLOR.0, PATHCOLOR.1, PATHCOLOR.2, PATHCOLOR.3);
+        let mut time_now = 0;
         for dot in self.model.dots.iter().rev().take(PATHLENGTH) {
-            context.line_to(dot.position.0, dot.position.1);
+            if dot.time > time_now {
+                time_now = dot.time
+            }
+            if time_now - dot.time < PATHFADINGTIME {
+                context.line_to(dot.position.0, dot.position.1);
+            } else {
+                break;
+            }
         }
         context.set_line_width(PATHWIDTH);
         context.stroke();
