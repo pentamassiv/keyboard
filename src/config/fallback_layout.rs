34cd627f07e61f494cf0c7bd15bc49f50068daab
grelltrier <grellr@hochschule-trier.de>
2020-10-16 02:59:26 +0200

    Added num button in fallback layout

--- src/config/fallback_layout.rs
+++ src/config/fallback_layout.rs
@@ -8,22 +8,22 @@ views:
         - \"q w e r t y u i o p\"
         - \"a s d f g h j k l\"
         - \"Shift_L_base z x c v b n m BackSpace\"
-        - \"show_numbers space . Return\"
+        - \"show_numbers show_symbols space . Return\"
     caps:
         - \"Q W E R T Y U I O P\"
         - \"A S D F G H J K L\"
         - \"Shift_L_caps Z X C V B N M BackSpace\"
-        - \"show_numbers space . Return\"
+        - \"show_numbers show_symbols space . Return\"
     numbers:
         - \"1 2 3 4 5 6 7 8 9 0\"
         - \"@ # $ % & - _ + ( )\"
         - \"show_symbols , \\\" ' : ; ! ? BackSpace\"
-        - \"show_letters space . Return\"
+        - \"show_letters show_symbols space . Return\"
     symbols:
         - \"~ ` | · √ π τ ÷ × ¶\"
         - \"© ® £ € ¥ ^ ° * { }\"
         - \"show_numbers_from_symbols \\\\ / < > = [ ] BackSpace\"
-        - \"show_letters space . Return\"
+        - \"show_letters show_numbers space . Return\"
 
 buttons:
     Shift_L_base:
@@ -40,7 +40,7 @@ buttons:
             short_press:
                 - switch_view: base
             long_press:
-                - switch_view: base
+                - switch_view: caps
         key_display:
             image: key-shift.svg
         outline: standard
