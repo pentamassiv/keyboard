8d0b5f7560da77b9c1ba754c2c5ba0aa0da8ef32
pentamassiv <pentamassiv@posteo.de>
2023-01-08 20:10:16 +0100

    Deny pedantic clippy lints

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#![deny(clippy::pedantic)]
+
 // Imports from other crates
 #[macro_use]
 extern crate bitflags;
